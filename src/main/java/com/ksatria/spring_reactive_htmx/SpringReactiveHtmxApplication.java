3163365b4bea838c1d0ce69e26df3fe85622944e
suryapradipta <suryapradipta8@gmail.com>
2024-03-05 16:09:20 +0800

    Initial commit

---
+++ src/main/java/com/ksatria/spring_reactive_htmx/SpringReactiveHtmxApplication.java
@@ -0,0 +1,13 @@
+package com.ksatria.spring_reactive_htmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveHtmxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveHtmxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksatria/spring_reactive_htmx/SpringReactiveHtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksatria.spring_reactive_htmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveHtmxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
